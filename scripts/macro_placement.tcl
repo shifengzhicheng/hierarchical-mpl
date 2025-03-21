5ce0a4d1ee36433ff5975a4ae8b4d2e4aa903073
Maple_of_Ten <zhiyuzheng7@gmail.com>
2025-03-21 17:42:43 +0800

    AutoDMP log

--- scripts/macro_placement.tcl
+++ scripts/macro_placement.tcl
@@ -1,5 +1,5 @@
 source load_design.tcl
 
-rtl_macro_placer -halo_width 10 -halo_height 10 -target_util 0.7
+rtl_macro_placer -halo_width 5 -halo_height 5 -target_util 0.7
 
 write_def ${case}_fp_HierRTLMP.def
